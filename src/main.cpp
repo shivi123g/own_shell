7225b3f25c0afc3da763ef4f5ee1e18f6ee91202
shivi123g <shivi23510@iiitd.ac.in>
2024-09-09 23:52:58 +0530

    hello

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,4 @@
-#include <iostream>
 
-int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
-  
-  std::string input;
-  std::getline(std::cin, input);
-}
+
+
